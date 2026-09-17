e4f183428ae1b7b4e8cf6e13fddb4e9aae39cd53
Ahmed badr <ab6281391@gmail.com>
2026-09-17 03:09:21 +0300

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Badr Academy JS is working!");
+const revealElements = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    revealElements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < window.innerHeight - 80) {
+            element.classList.add("reveal-active");
+        }
+    });
+}
+
+revealOnScroll();
+
+window.addEventListener("scroll", revealOnScroll);
